84f5cd9dac12b0104f1c688f9ce90a6880cadd71
Hukanzen <qazxswed1934@gmail.com>
2016-10-13 13:54:57 +0900

    2nd Commit

--- CUI/main.pl
+++ CUI/main.pl
@@ -5,27 +5,35 @@ use Win32::OLE;
 use Win32::Clipboard;
 
 my $clip=Win32::Clipboard();
-my $itunes =Win32::OLE->new("iTunes.Application") or die "iTunesƒIƒuƒWƒFƒNƒg‚ðì¬‚Å‚«‚Ü‚¹‚ñB";
+my $itunes =Win32::OLE->new("iTunes.Application") or die "iTunesã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆã‚’ä½œæˆã§ãã¾ã›ã‚“ã€‚";
 
-my $track = $itunes->CurrentTrack;
-
-if($track eq ''){
-	printf "not playing";
-	exit 1;
-} 
-
-#printf "Album : %s\n", $track->Album;
-#printf "Artist: %s\n", $track->Artist;
-#printf "Name  : %s\n", $track->Name;
-#printf "Time  : %s\n", $track->Time;
-
-my $text=sprintf("
-	#nowplaying \n 
+while(1){
+	my $track = $itunes->CurrentTrack;
+	
+	if($track eq ''){
+		printf "not playing";
+		exit 1;
+	} 
+	
+	#printf "Album : %s\n", $track->Album;
+	#printf "Artist: %s\n", $track->Artist;
+	#printf "Name  : %s\n", $track->Name;
+	#printf "Time  : %s\n", $track->Time;
+	
+	my $text=sprintf("#nowplaying \n 
 	Title \"%s\"\n 
 	Album \"%s\"\n 
 	Artist \"%s\"",$track->Name ,$track->Album,$track->Artist);
+	
+	
+	$clip->Set($text);
+	print $text;
+	
+	print "Copyed to Clipboard !\n";
 
-
-$clip->Set($text);
-
-print "Copyed to Clipboard !\n";
+	for(my $i=0;$i<4;$i++){
+		printf("%dmin\n",4-$i);
+		sleep(60);
+	}
+	print chr(7);
+}
