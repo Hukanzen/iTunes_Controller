ec819804cc1b90afc71d3428046bb97a460baec3
Hukanzen <qazxswed1934@gmail.com>
2016-10-14 10:30:18 +0900

    GUI list -> botton.

--- GUI/ribon.pl
+++ GUI/ribon.pl
@@ -5,6 +5,7 @@ use Encode qw(decode);
 use Win32::Clipboard;
 use Win32::OLE;
 use Tk;
+use Tk::Pane;
 
 #=== グローバル変数 ===#
 my $ENCODE='cp932';
@@ -26,10 +27,10 @@ my $top; #Main Window
 sub main(){
 
 	$top=MainWindow->new(-title=>'iTunes Control');
-	$top->minsize(250,200);
-
-	
+	$top->minsize(150,200);
+		
 	my $tk_nowPlaying=decode($ENCODE,$itunes->CurrentTrack->Name);
+
 	my $Playing_name=$top->Label(-fg=>'black',-textvariable=>\$tk_nowPlaying);
 
 #	$top->Label(-fg=>'black',-text=>'iTunes Control')->pack();
@@ -47,9 +48,14 @@ sub main(){
 	$top->Label(-fg=>'black',-text=>"\n")->pack();
 
 	$top->Button(-text=>'VolumeUP' ,-command=>[\&ctrl,'5'])->pack();
+	$top->Button(-text=>'VolumeDown' ,-command=>[\&ctrl,'6'])->pack();
 	$top->Label(-fg=>'black',-text=>"\n")->pack();
 
 	$top->Button(-text=>'list' ,-command=>[\&ctrl,'7'])->pack();
+	$top->Button(-text=>'select' ,-command=>[\&ctrl,'8'])->pack();
+	$top->Label(-fg=>'black',-text=>"\n")->pack();
+
+	$top->Button(-text=>'update' ,-command=>[\&update])->pack();
 	$top->Label(-fg=>'black',-text=>"\n")->pack();
 
 	$top->Button(-text=>'HELP' ,-command=>\&HELP)->pack();
@@ -74,16 +80,22 @@ sub ctrl(){
 		&copy;
 	}elsif($value==5){
 	#	$itunes->{SoundVolume}-=&Volume();
-		my $vlm=$top->Toplevel();
+		#my $vlm=$top->Toplevel();
 
-		$vlm->Label(-fg=>'black',-text=>'Volume UP')->pack();
+		#$vlm->Label(-fg=>'black',-text=>'Volume UP')->pack();
 
-		MainLoop();
-	}elsif($value==6){
+		#MainLoop();
 		$itunes->{SoundVolume}+=&Volume();
+	}elsif($value==6){
+		$itunes->{SoundVolume}-=&Volume();
 	}elsif($value==7){
 		&list;
+	}elsif($value==8){
+		&select;
 	}
+#	elsif($value==9){
+#		&update($_[1]);
+#	}
 	
 	printf($itunes->CurrentTrack->Name."\n");
 	return 0;
@@ -112,13 +124,14 @@ sub HELP(){
 }
 
 sub Volume(){
-	printf("Volume:");
-	my $vlm=<STDIN>;
-	if($vlm!~m!^\d+$!){
-		exit 2;
-	}
+	#printf("Volume:");
+	#my $vlm=<STDIN>;
+	#if($vlm!~m!^\d+$!){
+	#	exit 2;
+	#}
 
-	return $vlm*10;
+	#return $vlm*10;
+	return 2;
 }
 
 sub copy(){
@@ -144,41 +157,92 @@ sub copy(){
 
 sub list(){
 #	printf ("Playlist name : %s\n",$playlistName);
+	my $tk_playlistName=decode($ENCODE,$playlistName);
+
+	my $mytop=MainWindow->new(-title=>$tk_playlistName,-width=>150);
+	#my $mytop=MainWindow->new(-title=>$tk_playlistName);
+	#$mytop->minsize(150,200);
+	$mytop=$mytop->Scrolled('Frame',-scrollbars=>'e',-height=>800)->pack();
+	#my $mytop=MainWindow->new(-title=>$tk_playlistName,-width=>150,-height=>50);
 	my $tracks=$track_list->Tracks;
 
-	my $tk_playlistName=decode($ENCODE,$playlistName);
-	my $list_win=$top->Toplevel(-title=>$tk_playlistName,-width=>50,-height=>50);
-	#$list_win->maxsize(50,50);
-
-	my $lb=$list_win->Listbox(-width=>50,-height=>50);
-	my $sb=$list_win->Scrollbar(-orient=>'v',-command=>['yview',$lb]);
-
-	$lb->configure(-yscrollcommand=>['set',$sb]);
-	$lb->grid(-row=>1,-column=>0,-sticky=>'nsew');
-
-	$sb->grid(-row=>1,-column=>1,-sticky=>'ns');
 
 	for(my $i=1;$i<=($tracks->Count);$i++){
 		my $list_Item=$tracks->item($i);
 		my $Name=$list_Item->Name;
-
 		my $tk_Name=decode($ENCODE,$Name);
+
+		$mytop->Button(-text=>$tk_Name ,-command=>[\&cic_Play,$i,$tracks])->pack();
 		
-		$lb->insert('end',$tk_Name);
 	}
+	#=== ListBox ===#
+	#my $lb=$list_win->Listbox(-width=>50,-height=>50);
+	#my $sb=$list_win->Scrollbar(-orient=>'v',-command=>['yview',$lb]);
+
+	#$lb->configure(-yscrollcommand=>['set',$sb]);
+	#$lb->grid(-row=>1,-column=>0,-sticky=>'nsew');
+
+	#$sb->grid(-row=>1,-column=>1,-sticky=>'ns');
+
+	#for(my $i=1;$i<=($tracks->Count);$i++){
+	#	my $list_Item=$tracks->item($i);
+	#	my $Name=$list_Item->Name;
+
+	#	my $tk_Name=decode($ENCODE,$Name);
+	#	
+	#	$lb->insert('end',$i."-".$tk_Name);
+	#}
+	#=== ListBox ===#
+#	$list_win->bind("<Double-1>",\&get_music($tracks,$lb));
 #	$lb->see('start');
 	MainLoop();
 	
 
-#	my $msg=sprintf("番号を入力することで、曲選択ができます。\n");
-#	print decode($ENCODE,$msg);
 
+}
+sub cic_Play{
+	my ($cic,$tracks)=@_;
+	my $item=$tracks->item($cic);
+	$item->Play();
+}
+
+sub select{
+#	my $list_win=$top->Toplevel(-title=>$tk_playlistName,-width=>50,-height=>50);
+	my $tracks=$track_list->Tracks;
+	#my $msg=sprintf("番号を入力することで、曲選択ができます。\n");
+	#printf(decode($ENCODE,$msg));
+	print "type number in list,selection music.";
+   
+	my $cic=<STDIN>;
+
+	if($cic=~m!^\d+$!){
+		$tracks->item($cic)->Play();
+		#printf("%s\n",$tracks->item($cic)->Name);
+		printf("Title: %s\n",($itunes->CurrentTrack->Name));
+	}	
+}
+
+
+sub get_music{
+	my ($tracks,$lb)=@_;
+	my $cic=$lb->get('anchor');
+	print $cic;
+#
+#print $cic;
+#	print "aaa";
+	#$tracks->item($cic)->Play();
+}
+
+sub update{
+	my $top=MainWindow->new(-title=>'update');
+
+	my	$tk_nowPlaying=decode($ENCODE,$itunes->CurrentTrack->Name);
+	my	$Playing_name=$top->Label(-fg=>'black',-textvariable=>\$tk_nowPlaying);
+
+#	$top->Label(-fg=>'black',-text=>'iTunes Control')->pack();
+	$Playing_name->pack();
+	printf($itunes->CurrentTrack->Name);
 	
-#	my $cic=<STDIN>;
-
-#	if($cic=~m!^\d+$!){
-#		$tracks->item($cic)->Play();
-#		printf("%s\n",$tracks->item($cic)->Name);
-#	}
-
+	MainLoop();
+	return ;
 }
